5311de676d21569cc417e6780caa73a1c7bbf46b
tknott95 <tknott95@hotmail.com>
2022-07-29 16:34:34 -0600

    RabbitScraper

---
+++ RabbitScraper/CHANGELOG.md
@@ -0,0 +1,5 @@
+#RevisionhistoryforRabbitScraper
+
+##0.1.0.0--YYYY-mm-dd
+
+*Firstversion.Releasedonanunsuspectingworld.
---
+++ RabbitScraper/app/Main.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Control.Monad (forM_)
+
+
+-- for dl - this is normally modualrized out
+import           Control.Monad.Trans.Resource (runResourceT)
+import           Data.Conduit.Combinators     (sinkFile)
+import           Network.HTTP.Conduit         (parseRequest, responseStatus, responseStatus)
+import           Network.HTTP.Simple          (httpSink, getResponseStatusCode, httpLBS)
+
+
+
+readLines :: FilePath -> IO [String]
+readLines = fmap lines . readFile
+
+-- ipfsUrlStart = "http://infura-ipfs.io/ipfs/" :: String
+ipfsUrlStart = "http://image-optimizer.jpgstoreapis.com/" :: String
+
+
+addStrings :: String  -> String
+addStrings _x = ipfsUrlStart ++ _x
+
+-- returns pair of lists
+splitHalf :: [a] -> ([a], [a])
+splitHalf _list = splitAt ((length _list + 1) `div` 2) _list
+
+main :: IO ()
+main = do
+  ipfsEndings <- readLines "rabbits_ipfs_locs.txt"
+
+  let ifpsPair = splitHalf ipfsEndings
+
+  
+  
+  print $ length ipfsEndings
+
+  -- forM_ ipfsEndings $ \x ->   putStrLn $ show $ addStrings x
+
+
+  forM_ ipfsEndings $ \x ->   dlFile (addStrings x) "imgs" x 
+
+-- code below is from another project and needs to be modularized
+
+dYlw  = "\ESC[2;1;33m"
+bYlw  = "\ESC[1;33m"
+bCyan = "\ESC[0;1;36m"
+bRed     = "\ESC[0;1;31m"
+
+alt    = "\ESC[38;5;65m"
+alt2    = "\ESC[38;5;69m"
+
+clr    = "\ESC[0m"
+
+dlFile :: String -> String -> String  -> IO ()
+dlFile _url _filePath _fileName  = do
+  print _url
+  request <- parseRequest _url
+  resp2 <- httpLBS request
+
+  let status = getResponseStatusCode resp2
+  putStrLn $ "\nThe status code was: " ++ bYlw ++ show status ++ clr
+
+  if status == 200 then do
+    putStrLn (bCyan++"   DOWNLOADING: "++clr ++ _url ++ "\n      to: " ++  (_filePath++"/"++_fileName) ++ "\n")
+    runResourceT $ httpSink request $ \_ -> sinkFile (_filePath++"/"++_fileName)
+  else putStrLn (bRed++"   FAILED DOWNLOADING: "++clr ++ _url ++ "\n      FAILED to: " ++  (_filePath++"/"++_fileName) ++ "\n")
